1783480a6ec0fb5f496cbc3ba4e3197fc6086072
j.ossart <ossart.justin@gmail.com>
2016-02-12 11:21:02 +0100

    ajout controller list + creation du title dynamique pour les pages de list

---
+++ angular/app/view-list/moduleList.js
@@ -0,0 +1,28 @@
+angular.module('moduleList', [])
+	.config(function ($routeProvider) {	
+		$routeProvider.when('/mediaList', {
+			templateUrl: 'view-list/list-view.html',
+			controller: 'mediaCtrl',
+			controllerAs: 'media'
+		});
+
+
+		$routeProvider.when('/adherentList', {
+			templateUrl: 'view-list/list-view.html',
+			controller: 'adherentCtrl',
+			controllerAs: 'adherent'
+		});
+
+	})
+
+
+	.controller('mediaCtrl', function($rootScope) {
+		var controller = this;
+		$rootScope.pageTitle = 'list-media';
+	});
+
+
+	.controller('adherentCtrl', function($rootScope) {
+		var controller = this;
+		$rootScope.pageTitle = 'list-adherent';
+	});

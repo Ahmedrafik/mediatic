b0a46b542b4f246f9f6ad1319486280fe1a5676d
GuillaumeDINICOLA <guillaume.dinicola@gmail.com>
2016-02-12 10:45:57 +0100

    Conversion du toggle en Angular [a tester avec le reste]

---
+++ angular/app/js/script.js
@@ -0,0 +1,25 @@
+/* Fonction toggle */
+
+/* Sert à passer du mode edition à consultation */
+function uiToggleEdition()
+{
+    var bD = angular.element(document).find("body");
+	if(bD.hasClass("consultation")){
+		bD.removeClass("consultation");
+		bD.addClass("edition");
+	}
+	else{
+		bD.addClass("consultation");
+		bD.removeClass("edition");		
+	}
+}
+
+/* Bouchons */
+
+function triTable() {
+    console.log('TODO tri du tableau');	
+}
+
+function rechercheBDD(){
+    console.log('TODO recherche');
+}
